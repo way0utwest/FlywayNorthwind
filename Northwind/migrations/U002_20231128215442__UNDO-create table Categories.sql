860826c7ebbbc35c633b2f246fef60db7cadec5f
way0utwest <sjones@sqlservercentral.com>
2023-11-28 14:56:35 -0700

    First migration of categories

---
+++ Northwind/migrations/U002_20231128215442__UNDO-create table Categories.sql
@@ -0,0 +1,17 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping constraints from [dbo].[Categories]'
+GO
+ALTER TABLE [dbo].[Categories] DROP CONSTRAINT [PK_Categories]
+GO
+PRINT N'Dropping index [CategoryName] from [dbo].[Categories]'
+GO
+DROP INDEX [CategoryName] ON [dbo].[Categories]
+GO
+PRINT N'Dropping [dbo].[Categories]'
+GO
+DROP TABLE [dbo].[Categories]
+GO
+
---
+++ Northwind/migrations/V002_20231128215442__create table Categories.sql
@@ -0,0 +1,23 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Categories]'
+GO
+CREATE TABLE [dbo].[Categories]
+(
+[CategoryID] [int] NOT NULL IDENTITY(1, 1),
+[CategoryName] [nvarchar] (15) NOT NULL,
+[Description] [ntext] NULL,
+[Picture] [image] NULL
+)
+GO
+PRINT N'Creating primary key [PK_Categories] on [dbo].[Categories]'
+GO
+ALTER TABLE [dbo].[Categories] ADD CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED ([CategoryID])
+GO
+PRINT N'Creating index [CategoryName] on [dbo].[Categories]'
+GO
+CREATE NONCLUSTERED INDEX [CategoryName] ON [dbo].[Categories] ([CategoryName])
+GO
+
