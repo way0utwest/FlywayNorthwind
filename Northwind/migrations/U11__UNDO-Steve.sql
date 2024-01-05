d55d2ac007ef3cb70f004837850a8878a441aaf7
way0utwest <sjones@sqlservercentral.com>
2024-01-05 16:56:13 -0700

    #2 create orders table

---
+++ Northwind/migrations/U11__UNDO-Steve.sql
@@ -0,0 +1,66 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping foreign keys from [dbo].[Orders]'
+GO
+ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_Orders_Customers]
+GO
+ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_Orders_Employees]
+GO
+ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [FK_Orders_Shippers]
+GO
+PRINT N'Dropping constraints from [dbo].[Orders]'
+GO
+ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [PK_Orders]
+GO
+PRINT N'Dropping constraints from [dbo].[Orders]'
+GO
+ALTER TABLE [dbo].[Orders] DROP CONSTRAINT [DF_Orders_Freight]
+GO
+PRINT N'Dropping index [CustomerID] from [dbo].[Orders]'
+GO
+DROP INDEX [CustomerID] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [CustomersOrders] from [dbo].[Orders]'
+GO
+DROP INDEX [CustomersOrders] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [EmployeeID] from [dbo].[Orders]'
+GO
+DROP INDEX [EmployeeID] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [EmployeesOrders] from [dbo].[Orders]'
+GO
+DROP INDEX [EmployeesOrders] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [OrderDate] from [dbo].[Orders]'
+GO
+DROP INDEX [OrderDate] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [ShippedDate] from [dbo].[Orders]'
+GO
+DROP INDEX [ShippedDate] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [ShippersOrders] from [dbo].[Orders]'
+GO
+DROP INDEX [ShippersOrders] ON [dbo].[Orders]
+GO
+PRINT N'Dropping index [ShipPostalCode] from [dbo].[Orders]'
+GO
+DROP INDEX [ShipPostalCode] ON [dbo].[Orders]
+GO
+PRINT N'Dropping [dbo].[Orders]'
+GO
+DROP TABLE [dbo].[Orders]
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
---
+++ Northwind/migrations/V11__Create table orders.sql
@@ -0,0 +1,78 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Orders]'
+GO
+CREATE TABLE [dbo].[Orders]
+(
+[OrderID] [int] NOT NULL IDENTITY(1, 1),
+[CustomerID] [nchar] (5) NULL,
+[EmployeeID] [int] NULL,
+[OrderDate] [datetime] NULL,
+[RequiredDate] [datetime] NULL,
+[ShippedDate] [datetime] NULL,
+[ShipVia] [int] NULL,
+[Freight] [money] NULL CONSTRAINT [DF_Orders_Freight] DEFAULT ((0)),
+[ShipName] [nvarchar] (40) NULL,
+[ShipAddress] [nvarchar] (60) NULL,
+[ShipCity] [nvarchar] (15) NULL,
+[ShipRegion] [nvarchar] (15) NULL,
+[ShipPostalCode] [nvarchar] (10) NULL,
+[ShipCountry] [nvarchar] (15) NULL
+)
+GO
+PRINT N'Creating primary key [PK_Orders] on [dbo].[Orders]'
+GO
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([OrderID])
+GO
+PRINT N'Creating index [CustomerID] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [CustomerID] ON [dbo].[Orders] ([CustomerID])
+GO
+PRINT N'Creating index [CustomersOrders] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [CustomersOrders] ON [dbo].[Orders] ([CustomerID])
+GO
+PRINT N'Creating index [EmployeeID] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [EmployeeID] ON [dbo].[Orders] ([EmployeeID])
+GO
+PRINT N'Creating index [EmployeesOrders] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [EmployeesOrders] ON [dbo].[Orders] ([EmployeeID])
+GO
+PRINT N'Creating index [OrderDate] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [OrderDate] ON [dbo].[Orders] ([OrderDate])
+GO
+PRINT N'Creating index [ShippedDate] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [ShippedDate] ON [dbo].[Orders] ([ShippedDate])
+GO
+PRINT N'Creating index [ShipPostalCode] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [ShipPostalCode] ON [dbo].[Orders] ([ShipPostalCode])
+GO
+PRINT N'Creating index [ShippersOrders] on [dbo].[Orders]'
+GO
+CREATE NONCLUSTERED INDEX [ShippersOrders] ON [dbo].[Orders] ([ShipVia])
+GO
+PRINT N'Adding foreign keys to [dbo].[Orders]'
+GO
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_Orders_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_Orders_Employees] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID])
+GO
+ALTER TABLE [dbo].[Orders] ADD CONSTRAINT [FK_Orders_Shippers] FOREIGN KEY ([ShipVia]) REFERENCES [dbo].[Shippers] ([ShipperID])
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
