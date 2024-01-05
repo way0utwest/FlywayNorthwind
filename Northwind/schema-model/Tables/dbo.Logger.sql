1ab93a3214e737ea9419c50b909fb4b8d785f550
way0utwest <sjones@sqlservercentral.com>
2024-01-05 16:45:31 -0700

    create table logger with migration

---
+++ Northwind/schema-model/Tables/dbo.Logger.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Logger]
+(
+[LogID] [int] NOT NULL,
+[LogDate] [datetime] NULL CONSTRAINT [DF__Logger__LogDate__02FC7413] DEFAULT (sysutcdatetime()),
+[LogMsg] [varchar] (2000) NULL
+)
+GO
+ALTER TABLE [dbo].[Logger] ADD CONSTRAINT [LoggerPK] PRIMARY KEY CLUSTERED ([LogID])
+GO
