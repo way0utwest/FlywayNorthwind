b9aeffdf41e338fd563cd1955ffe4fba5bf97398
way0utwest <sjones@sqlservercentral.com>
2024-01-05 17:05:15 -0700

    Create table products #1

---
+++ Northwind/migrations/U12__UNDO-Steve.sql
@@ -0,0 +1,84 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping foreign keys from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [FK_Products_Suppliers]
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [FK_Products_Categories]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [CK_Products_UnitPrice]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [CK_UnitsInStock]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [CK_UnitsOnOrder]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [CK_ReorderLevel]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [PK_Products]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF_Products_UnitPrice]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF_Products_UnitsInStock]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF_Products_UnitsOnOrder]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF_Products_ReorderLevel]
+GO
+PRINT N'Dropping constraints from [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] DROP CONSTRAINT [DF_Products_Discontinued]
+GO
+PRINT N'Dropping index [ProductName] from [dbo].[Products]'
+GO
+DROP INDEX [ProductName] ON [dbo].[Products]
+GO
+PRINT N'Dropping index [SupplierID] from [dbo].[Products]'
+GO
+DROP INDEX [SupplierID] ON [dbo].[Products]
+GO
+PRINT N'Dropping index [SuppliersProducts] from [dbo].[Products]'
+GO
+DROP INDEX [SuppliersProducts] ON [dbo].[Products]
+GO
+PRINT N'Dropping index [CategoriesProducts] from [dbo].[Products]'
+GO
+DROP INDEX [CategoriesProducts] ON [dbo].[Products]
+GO
+PRINT N'Dropping index [CategoryID] from [dbo].[Products]'
+GO
+DROP INDEX [CategoryID] ON [dbo].[Products]
+GO
+PRINT N'Dropping [dbo].[Products]'
+GO
+DROP TABLE [dbo].[Products]
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
---
+++ Northwind/migrations/V12__create products table #1.sql
@@ -0,0 +1,70 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Products]'
+GO
+CREATE TABLE [dbo].[Products]
+(
+[ProductID] [int] NOT NULL IDENTITY(1, 1),
+[ProductName] [nvarchar] (40) NOT NULL,
+[SupplierID] [int] NULL,
+[CategoryID] [int] NULL,
+[QuantityPerUnit] [nvarchar] (20) NULL,
+[UnitPrice] [money] NULL CONSTRAINT [DF_Products_UnitPrice] DEFAULT ((0)),
+[UnitsInStock] [smallint] NULL CONSTRAINT [DF_Products_UnitsInStock] DEFAULT ((0)),
+[UnitsOnOrder] [smallint] NULL CONSTRAINT [DF_Products_UnitsOnOrder] DEFAULT ((0)),
+[ReorderLevel] [smallint] NULL CONSTRAINT [DF_Products_ReorderLevel] DEFAULT ((0)),
+[Discontinued] [bit] NOT NULL CONSTRAINT [DF_Products_Discontinued] DEFAULT ((0))
+)
+GO
+PRINT N'Creating primary key [PK_Products] on [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID])
+GO
+PRINT N'Creating index [CategoriesProducts] on [dbo].[Products]'
+GO
+CREATE NONCLUSTERED INDEX [CategoriesProducts] ON [dbo].[Products] ([CategoryID])
+GO
+PRINT N'Creating index [CategoryID] on [dbo].[Products]'
+GO
+CREATE NONCLUSTERED INDEX [CategoryID] ON [dbo].[Products] ([CategoryID])
+GO
+PRINT N'Creating index [ProductName] on [dbo].[Products]'
+GO
+CREATE NONCLUSTERED INDEX [ProductName] ON [dbo].[Products] ([ProductName])
+GO
+PRINT N'Creating index [SupplierID] on [dbo].[Products]'
+GO
+CREATE NONCLUSTERED INDEX [SupplierID] ON [dbo].[Products] ([SupplierID])
+GO
+PRINT N'Creating index [SuppliersProducts] on [dbo].[Products]'
+GO
+CREATE NONCLUSTERED INDEX [SuppliersProducts] ON [dbo].[Products] ([SupplierID])
+GO
+PRINT N'Adding constraints to [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [CK_Products_UnitPrice] CHECK (([UnitPrice]>=(0)))
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [CK_UnitsInStock] CHECK (([UnitsInStock]>=(0)))
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [CK_UnitsOnOrder] CHECK (([UnitsOnOrder]>=(0)))
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [CK_ReorderLevel] CHECK (([ReorderLevel]>=(0)))
+GO
+PRINT N'Adding foreign keys to [dbo].[Products]'
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [FK_Products_Suppliers] FOREIGN KEY ([SupplierID]) REFERENCES [dbo].[Suppliers] ([SupplierID])
+GO
+ALTER TABLE [dbo].[Products] ADD CONSTRAINT [FK_Products_Categories] FOREIGN KEY ([CategoryID]) REFERENCES [dbo].[Categories] ([CategoryID])
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
