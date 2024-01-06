951dc3e9589ee9be653a72d2041572cb593e31da
way0utwest <sjones@sqlservercentral.com>
2024-01-05 17:13:23 -0700

    create table orderdetails #2

---
+++ Northwind/migrations/U13__UNDO-Steve.sql
@@ -0,0 +1,52 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Dropping foreign keys from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [FK_Order_Details_Orders]
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [FK_Order_Details_Products]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [CK_UnitPrice]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [CK_Quantity]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [CK_Discount]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [PK_Order_Details]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [DF_Order_Details_UnitPrice]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [DF_Order_Details_Quantity]
+GO
+PRINT N'Dropping constraints from [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] DROP CONSTRAINT [DF_Order_Details_Discount]
+GO
+PRINT N'Dropping [dbo].[Order Details]'
+GO
+DROP TABLE [dbo].[Order Details]
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
---
+++ Northwind/migrations/V13__create table orderdetails #2.sql
@@ -0,0 +1,43 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[Order Details]'
+GO
+CREATE TABLE [dbo].[Order Details]
+(
+[OrderID] [int] NOT NULL,
+[ProductID] [int] NOT NULL,
+[UnitPrice] [money] NOT NULL CONSTRAINT [DF_Order_Details_UnitPrice] DEFAULT ((0)),
+[Quantity] [smallint] NOT NULL CONSTRAINT [DF_Order_Details_Quantity] DEFAULT ((1)),
+[Discount] [real] NOT NULL CONSTRAINT [DF_Order_Details_Discount] DEFAULT ((0))
+)
+GO
+PRINT N'Creating primary key [PK_Order_Details] on [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [PK_Order_Details] PRIMARY KEY CLUSTERED ([OrderID], [ProductID])
+GO
+PRINT N'Adding constraints to [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [CK_UnitPrice] CHECK (([UnitPrice]>=(0)))
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [CK_Quantity] CHECK (([Quantity]>(0)))
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [CK_Discount] CHECK (([Discount]>=(0) AND [Discount]<=(1)))
+GO
+PRINT N'Adding foreign keys to [dbo].[Order Details]'
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [FK_Order_Details_Orders] FOREIGN KEY ([OrderID]) REFERENCES [dbo].[Orders] ([OrderID])
+GO
+ALTER TABLE [dbo].[Order Details] ADD CONSTRAINT [FK_Order_Details_Products] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ProductID])
+GO
+
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS, NOCOUNT ON
+GO
+SET DATEFORMAT YMD
+GO
+SET XACT_ABORT ON
+GO
+
